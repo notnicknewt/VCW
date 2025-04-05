dcee5a36f16abff5949d47fc6dbfb6f739aa83a5
notnicknewt <pti.nick@gmail.com>
2025-04-05 23:47:09 +0100

    Fix layout children prop typing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
-export default function RootLayout({ children }) {
+import React from 'react';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head />
