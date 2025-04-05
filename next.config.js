9a0564e7218953926675d96214fa493ce3efad39
notnicknewt <pti.nick@gmail.com>
2025-04-06 00:32:06 +0100

    Add redirect from / to /dashboard

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: false, // Set to true if you want a 308 permanent redirect
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
