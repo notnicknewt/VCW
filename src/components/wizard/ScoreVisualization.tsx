ccfe768060b9d8ca67d9e7e6f196ce497553dd0a
notnicknewt <pti.nick@gmail.com>
2025-04-06 11:55:56 +0100

    Add missing ScoreVisualization component placeholder

---
+++ src/components/wizard/ScoreVisualization.tsx
@@ -0,0 +1,8 @@
+export default function ScoreVisualization() {
+  return (
+    <div className="score-visualization-placeholder">
+      {/* This will display a visualisation of the score in the future */}
+      <p>Score visualisation coming soon...</p>
+    </div>
+  );
+}
